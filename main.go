f635b7cf26b6d19af3040ec9c0e6ff972b21a0f1
Argi Fatur <argifatur@gmail.com>
2023-07-24 11:46:03 +0800

    feat: jwt auth, generate token

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"bwastartup/auth"
 	"bwastartup/handler"
 	"bwastartup/user"
+	"fmt"
 	"log"
 
 	"github.com/gin-gonic/gin"
@@ -20,8 +22,9 @@ func main() {
 
 	userRepository := user.NewRepository(db)
 	userService := user.NewService(userRepository)
-	userService.SaveAvatar(2, "images/1-profile.png")
-	userHandler := handler.NewUserHandler(userService)
+	authService := auth.NewService()
+	fmt.Println(authService.GenerateToken(1001))
+	userHandler := handler.NewUserHandler(userService, authService)
 
 	router := gin.Default()
 	api := router.Group("/api/v1")
